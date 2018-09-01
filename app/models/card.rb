6689aedb24718e1a752b7563c4bb2311a7dc81a4
Lucas Kuhn <lucasalexkuhn@gmail.com>
2018-09-01 16:47:49 -0300

    sanitize

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,3 +1,8 @@
 class Card < ApplicationRecord
-  
+  before_validation :sanitize_content, :on => :create
+
+  def sanitize_content
+    self.description = ActionController::Base.helpers.sanitize(self.description)
+  end
+
 end
