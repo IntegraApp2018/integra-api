89654267cef5aaae5f0d57087c57d0081dcae71b
Lucas Kuhn <lucasalexkuhn@gmail.com>
2018-09-01 18:18:16 -0300

    removes token

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,31 +1,33 @@
 class CategoriesController < ApplicationController
-    def index
-      @categories = Category.all
-      render status: 200, json: @categories.to_json
-    end
+  skip_before_action :verify_authenticity_token
+  
+  def index
+    @categories = Category.all
+    render status: 200, json: @categories.to_json
+  end
 
-    def list
-      @categories = Category.all.map {|category| {name: category.name, id:category.id} }
-      render status: 200, json: @categories
-    end
+  def list
+    @categories = Category.all.map {|category| {name: category.name, id:category.id} }
+    render status: 200, json: @categories
+  end
 
-    def new
-      @category = Category.new
-    end
+  def new
+    @category = Category.new
+  end
 
-    def create
-      @category = Category.create(category_params)
-      render status: 200, json: @category.to_json
-    end
+  def create
+    @category = Category.create(category_params)
+    render status: 200, json: @category.to_json
+  end
 
-    def show
-      @category = Category.find(params[:id])
-      render status: 200, json: @categories.to_json
-    end
+  def show
+    @category = Category.find(params[:id])
+    render status: 200, json: @categories.to_json
+  end
 
-    private
+  private
 
-    def category_params
-      params.permit(:name)
-    end
+  def category_params
+    params.permit(:name)
+  end
 end
