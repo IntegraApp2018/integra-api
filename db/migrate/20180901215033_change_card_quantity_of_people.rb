bd42beae5159155bf59b3fa459e794bee21a8bb8
Lucas Kuhn <lucasalexkuhn@gmail.com>
2018-09-02 07:05:36 -0300

    adds seed

--- db/migrate/20180901215033_change_card_quantity_of_people.rb
+++ db/migrate/20180901215033_change_card_quantity_of_people.rb
@@ -1,6 +1,6 @@
 class ChangeCardQuantityOfPeople < ActiveRecord::Migration[5.2]
   def change
-    rename_column :cards, :quantity_of_people, :people_interested
+    rename_column :cards, :quantity_of_people, :people_interested, default: 0
     add_column :cards, :people_needed, :integer
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,51 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-lucas = User.create(name:"Lucas Silva", email:"lucassilva@teste.com")
-Card.create(title:"")
+lucas = User.create(name:"Lucas Silva", email:"lucas@teste.com", password: "123123")
+luana = User.create(name:"Luana Souza", email:"luana@teste.com", password: "123123")
+leia = User.create(name:"Princesa Leia", email:"leia@teste.com", password: "123123")
+yoda = User.create(name:"Mestre Yoda", email:"yoda@teste.com", password: "123123")
+anakin = User.create(name:"Anakin Skywalker", email:"anakin@teste.com", password: "123123")
+
+transporte = Category.create(name:"Transporte")
+infraestrutura = Category.create(name:"Infraestrutura")
+animais = Category.create(name:"Animais")
+
+card = Card.create(
+  title:"Carona Solidária",
+  description:"Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
+  category_id: transporte.id,
+  owner_id: lucas.id,
+  people_needed: 3,
+  time: DateTime.parse("2018-10-10T10:00:00+07:00")
+)
+
+card.participants.create(user_id:leia)
+card.participants.create(user_id:yoda)
+card.participants.create(user_id:anakin)
+
+card = Card.create(
+  title:"Pintura do ginásio da escola",
+  description:"Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
+  category_id: infraestrutura.id,
+  owner_id: luana.id,
+  people_needed: 2,
+  time: DateTime.parse("2018-10-10T10:00:00+07:00")
+)
+
+card.participants.create(user_id:leia)
+card.participants.create(user_id:yoda)
+card.participants.create(user_id:anakin)
+
+card = Card.create(
+  title:"Feira de adoção",
+  description:"Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
+  category_id: animais.id,
+  owner_id: luana.id,
+  people_needed: 2,
+  time: DateTime.parse("2018-10-10T10:00:00+07:00")
+)
+
+card.participants.create(user_id:leia)
+card.participants.create(user_id:yoda)
+card.participants.create(user_id:anakin)
